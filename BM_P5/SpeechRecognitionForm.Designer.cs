2fffa6e2f75997d5ebf9c413f84214c13398b536
robertp97 <robertp97@interia.pl>
2019-04-26 11:37:21 +0200

    Initial layout settings, libs imported and tested, renamed form file.

---
+++ BM_P5/SpeechRecognitionForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BM_P5
+{
+    partial class SpeechRecognitionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.waveViewer2 = new NAudio.Gui.WaveViewer();
+            this.waveViewer1 = new NAudio.Gui.WaveViewer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.track1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.track2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.waveViewer2);
+            this.panel1.Controls.Add(this.waveViewer1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(49, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "loadtrack2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "loadtrack1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Track 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Track 1";
+            // 
+            // waveViewer2
+            // 
+            this.waveViewer2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.waveViewer2.Location = new System.Drawing.Point(25, 351);
+            this.waveViewer2.Name = "waveViewer2";
+            this.waveViewer2.SamplesPerPixel = 128;
+            this.waveViewer2.Size = new System.Drawing.Size(750, 90);
+            this.waveViewer2.StartPosition = ((long)(0));
+            this.waveViewer2.TabIndex = 1;
+            this.waveViewer2.WaveStream = null;
+            // 
+            // waveViewer1
+            // 
+            this.waveViewer1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.waveViewer1.Location = new System.Drawing.Point(25, 227);
+            this.waveViewer1.Name = "waveViewer1";
+            this.waveViewer1.SamplesPerPixel = 128;
+            this.waveViewer1.Size = new System.Drawing.Size(750, 90);
+            this.waveViewer1.StartPosition = ((long)(0));
+            this.waveViewer1.TabIndex = 0;
+            this.waveViewer1.WaveStream = null;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.track1ToolStripMenuItem,
+            this.track2ToolStripMenuItem});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // track1ToolStripMenuItem
+            // 
+            this.track1ToolStripMenuItem.Name = "track1ToolStripMenuItem";
+            this.track1ToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.track1ToolStripMenuItem.Text = "Track 1";
+            // 
+            // track2ToolStripMenuItem
+            // 
+            this.track2ToolStripMenuItem.Name = "track2ToolStripMenuItem";
+            this.track2ToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.track2ToolStripMenuItem.Text = "Track 2";
+            // 
+            // SpeechRecognitionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SpeechRecognitionForm";
+            this.Text = "Speech recognition - DTW";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private NAudio.Gui.WaveViewer waveViewer2;
+        private NAudio.Gui.WaveViewer waveViewer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem track1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem track2ToolStripMenuItem;
+    }
+}
+
---
+++ BM_P5/SpeechRecognitionForm.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Windows.Forms;
+using AForge.Math;
+namespace BM_P5
+{
+    public partial class SpeechRecognitionForm : Form
+    {
+        public SpeechRecognitionForm()
+        {
+            InitializeComponent();
+        }
+        public void test_method()
+        {
+            //MathNet.Numerics.IntegralTransforms.Fourier.Forward; działa niezbyt XD
+            //AForge
+            Complex[] data = new Complex[1];
+            FourierTransform.Direction direction = FourierTransform.Direction.Forward;
+            FourierTransform.FFT(data, direction);
+        }
+    }
+
+}
